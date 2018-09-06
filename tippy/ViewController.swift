2d35b4450c45a6602d4b34bf67a8dbb2ccfc774b
Marc Edwin Theolien <marcedwintheolien@gmail.com>
2018-09-06 10:26:42 -0400

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Marc Edwin Theolien on 9/6/18.
+//  Copyright © 2018 Marc Edwin Theolien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
